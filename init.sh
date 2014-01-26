3e10df894d94b91110654bd1a68d42093599ccb4
Homam <bluesnowball@gmail.com>
2014-01-26 15:05:55 +0400

    select2 in init

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 mkdir public
 mkdir public/javascript
 mkdir public/javascript/libs
+mkdir public/javascript/libs/select2
 curl http://code.jquery.com/jquery-2.0.3.js > public/javascript/libs/jquery2.js
 curl https://raw.github.com/mbostock/d3/master/d3.js > public/javascript/libs/d3.js
 curl -L http://preludels.com/prelude-browser.js > public/javascript/libs/prelude-browser.js
+curl -L https://raw.github.com/ivaynberg/select2/master/select2.js > public/javascript/libs/select2/select2.min.js
+curl -L https://raw.github.com/ivaynberg/select2/master/select2.css > public/javascript/libs/select2/select2.css
